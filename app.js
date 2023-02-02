3262e0498f7139acb8e5a3e2d9420f88bdf8ef78
nadine <nadine.portilla@inetum.com>
2023-02-02 15:15:33 -0500

    Webserver

---
+++ app.js
@@ -0,0 +1,53 @@
+require('dotenv').config();
+const express = require('express')
+var hbs = require('hbs');
+
+
+
+const app = express()
+const port = process.env.PORT;
+
+//Handlebars
+app.set('view engine','hbs'); 
+hbs.registerPartials(__dirname + '/views/partials', function (err) {});
+
+//servir contenido estático
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    res.render('home',{
+        nombre:'Neydi Portilla',
+        titulo:'Curso de Node'
+    });
+});
+
+app.get('/generic', function (req, res) {
+    res.render('generic',{
+        nombre:'Generic-Neydi Portilla',
+        titulo:'Curso de Node'
+    });
+});
+
+app.get('/elements', function (req, res) {
+    res.render('elements',{
+        nombre:'Elements-Neydi Portilla',
+        titulo:'Curso de Node'
+    });
+});
+
+// app.get('/generic', function (req, res) {
+//     res.sendFile(__dirname + '/public/generic.html');
+// });
+
+// app.get('/elements', function (req, res) {
+//     res.sendFile(__dirname + '/public/elements.html');
+// });
+
+// app.get('*', function (req, res) {
+//     res.sendFile(__dirname + '/public/404.html');
+// });
+
+app.listen(port,()=>{
+    console.log(`Example app listening at http://localhost:${port}`);
+})
---
+++ app_old.js
@@ -0,0 +1,25 @@
+const http = require("http");
+
+http.createServer((req,res) => {
+
+     console.log(req);
+
+     res.setHeader('Content-Disposition','attachment; filename=lista.csv');
+     res.writeHead(200,{'Content-Type':'application/csv'});
+
+     const persona = {
+        id: 1,
+        nombre:'Fernando'
+     }
+
+     res.write( 'id,nombre\n');
+     res.write( '1,Fernando\n');
+     res.write( '2,Maria\n');
+     res.write( '3,Juan\n');
+     res.write( '4,Pedro\n');
+     res.end();
+})
+.listen(8080);
+
+console.log('Escuchando en el puerto', 8080);
+
